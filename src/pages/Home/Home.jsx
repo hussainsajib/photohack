57730667e89dff5ccd49a9d986cc0dc680cf5699
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-07-06 14:02:34 -0400

    gallery working

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { InputGroup, FormControl } from "react-bootstrap";
 import axios from "axios";
+import Gallery from "react-grid-gallery";
 
 class Home extends Component {
     constructor(props) {
@@ -31,25 +32,23 @@ class Home extends Component {
         };
         axios
             .get(
-                "https://api.unsplash.com/search/photos?page=1&query=office",
+                `https://api.unsplash.com/search/photos?page=1&query=${this.state.searchString}&per_page=30`,
                 options
             )
             .then((res) => res.data.results)
             .then((results) => {
                 const data = results.map((item) => {
                     const parsedData = {};
-                    parsedData.src = item.urls.thumb;
-                    parsedData.width = item.width;
-                    parsedData.height = item.height;
-                    parsedData.large = item.urls.regular;
+                    parsedData.thumbnail = item.urls.thumb;
+                    parsedData.thumbnailWidth = item.width;
+                    parsedData.thumbnailHeight = item.height;
+                    parsedData.src = item.urls.regular;
                     return parsedData;
                 });
                 return data;
             })
             .then((parsedData) => {
-                this.setState({ searchResult: parsedData }, () =>
-                    console.log(this.state.searchResult)
-                );
+                this.setState({ searchResult: parsedData });
             })
             .catch((err) => console.error(err));
     }
@@ -66,6 +65,9 @@ class Home extends Component {
                         onKeyPress={this.handleChange}
                     />
                 </InputGroup>
+                {this.state.searchResult.length === 0 ? null : (
+                    <Gallery images={this.state.searchResult} />
+                )}
             </div>
         );
     }
